03bcfbc824e914be6395a3b0ff846a9c3861b083
petter johansson <146840972+PetterJohanssonTilia@users.noreply.github.com>
2024-07-05 12:53:39 +0000

    Build: base+index, created and linked css

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,9 @@ from .models import Post
 class PostList(generic.ListView):
     #model = Post
     #template_name = 'blog/post_list.html' #Used if we didnt name the model post_list.html
-    queryset = Post.objects.filter(status=1)
     #queryset = Post.objects.all()
     #queryset = Post.objects.filter(author=2)
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
+    
